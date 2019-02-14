c4af2abf966579f42e2a03d01ae1088b203a6365
Patrick-Wynne <wynne.patrickt@gmail.com>
2019-02-13 19:31:06 -0500

    did resturant

---
+++ Week02/ClassesIntro/Resturant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Resturant
+{
+    public string Name;
+    public string FoodType;
+    public double Rating;
+    public List<string> menuList = new List<string>() { "fries", "hotdog", "hamburger" };
+
+    public void showMenu()
+    {
+        
+    }
+}
