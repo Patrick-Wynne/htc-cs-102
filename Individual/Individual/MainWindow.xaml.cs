f6c2951ce4ee59a0f668a18885321ff768b55a2a
Patrick-Wynne <wynne.patrickt@gmail.com>
2019-04-03 19:30:46 -0400

    did shapes

---
+++ Individual/Individual/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Individual
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Rectangle myRect = new Rectangle();
+            //myRect.Height = 100;
+            //myRect.Width = 100;
+            //myRect.Fill = Brushes.Red;
+            //myRect.Stroke = Brushes.Black;
+            //myRect.StrokeThickness = 2;
+            //Canvas.SetLeft(myRect, 100);
+            //Canvas.SetTop(myRect, 100);
+            //myCanvas.Children.Add(myRect);
+
+            //Diamond myDiam = new Diamond(Brushes.Black, Brushes.Red, 2, 100, 100);
+            //myCanvas.Children.Add(myDiam);
+
+            //Triangle myTri = new Triangle(Brushes.Black, Brushes.Red, 2, 100, 100, false);
+            //myCanvas.Children.Add(myTri);
+
+            Pentagon myPent = new Pentagon(Brushes.Black, Brushes.Red, 2, 100, 90);
+            myCanvas.Children.Add(myPent);
+        }
+    }
+}
---
+++ Individual/Individual/Triangle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Individual
+{
+    class Triangle : Shape
+    {
+        public bool isRightTriangle;
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                if(isRightTriangle == true)
+                {
+                    PathFigure myPathFigure = new PathFigure();
+                    myPathFigure.StartPoint = new Point(0, 0);
+
+                    LineSegment segment1 = new LineSegment();
+                    segment1.Point = new Point(Width, Height);
+                    LineSegment segment2 = new LineSegment();
+                    segment2.Point = new Point(0, Height);
+                    LineSegment segment3 = new LineSegment();
+                    segment3.Point = new Point(0, 0);
+                    PathSegmentCollection myPathSegmentCollection = new PathSegmentCollection();
+                    myPathSegmentCollection.Add(segment1);
+                    myPathSegmentCollection.Add(segment2);
+                    myPathSegmentCollection.Add(segment3);
+
+                    myPathFigure.Segments = myPathSegmentCollection;
+
+                    PathFigureCollection myPathFigureCollection = new PathFigureCollection();
+                    myPathFigureCollection.Add(myPathFigure);
+
+                    PathGeometry myPathGeometry = new PathGeometry();
+                    myPathGeometry.Figures = myPathFigureCollection;
+
+                    return myPathGeometry;
+                }
+                else
+                {
+                    PathFigure myPathFigure = new PathFigure();
+                    myPathFigure.StartPoint = new Point(Width / 2, 0);
+
+                    LineSegment segment1 = new LineSegment();
+                    segment1.Point = new Point(Width, Height);
+                    LineSegment segment2 = new LineSegment();
+                    segment2.Point = new Point(0, Height);
+                    LineSegment segment3 = new LineSegment();
+                    segment3.Point = new Point(Width / 2, 0);
+                    PathSegmentCollection myPathSegmentCollection = new PathSegmentCollection();
+                    myPathSegmentCollection.Add(segment1);
+                    myPathSegmentCollection.Add(segment2);
+                    myPathSegmentCollection.Add(segment3);
+
+                    myPathFigure.Segments = myPathSegmentCollection;
+
+                    PathFigureCollection myPathFigureCollection = new PathFigureCollection();
+                    myPathFigureCollection.Add(myPathFigure);
+
+                    PathGeometry myPathGeometry = new PathGeometry();
+                    myPathGeometry.Figures = myPathFigureCollection;
+
+                    return myPathGeometry;
+                }
+            }
+        }
+        public Triangle(Brush strokeColor, Brush fillColor, double strokeThickness, int width, int height, bool isRightTriangle)
+        {
+            this.Stroke = strokeColor;
+            this.Fill = fillColor;
+            this.Width = width;
+            this.Height = height;
+            this.StrokeThickness = strokeThickness;
+            this.isRightTriangle = isRightTriangle;
+        }
+    }
+}
