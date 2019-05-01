ac06cf18be126f2c328fe9b6d2169fe63b2d1c3d
Patrick-Wynne <47403848+Patrick-Wynne@users.noreply.github.com>
2019-05-01 16:37:47 -0400

    Update MainWindow.xaml.cs

--- Final/Final/MainWindow.xaml.cs
+++ Final/Final/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +30,6 @@ namespace Final
             comboBoxShapeSelect.SelectedIndex = 1;
             comboBoxTypeSelect.SelectedIndex = 0;
         }
-
         private void comboBoxTypeSelect_SelectionChanged(object sender, EventArgs e)
         {
             var select = comboBoxTypeSelect.SelectedIndex.ToString();
@@ -69,18 +69,25 @@ namespace Final
                 myRect.Stroke = Brushes.Black;
                 myRect.StrokeThickness = 2;
                 mainCanvas.Children.Add(myRect);
+                //info.Content = "hp:200\nattack:30";
+                //playerHealth.Maximum = 200;
+                //playerHealth.Value = 200;
             }
             else if (select == "1")
             {
                 mainCanvas.Children.Clear();
                 Triangle myTri = new Triangle(Brushes.Black, color, 2, 50, 50);
                 mainCanvas.Children.Add(myTri);
+                //info.Content = "hp:150\nattack:45";
+                //playerHealth.Maximum = 150;
+                //playerHealth.Value = 150;
             }
             else if (select == "2")
             {
                 mainCanvas.Children.Clear();
                 Circle myCir = new Circle(Brushes.Black, color, 2, 50, 50);
                 mainCanvas.Children.Add(myCir);
+                //info.Content = "hp:100\nattack:60";
             }
         }
         private void addSquare(Brush color)
@@ -108,25 +115,52 @@ namespace Final
         {
             clearCanv.Children.Clear();
             Random shapeColor = new Random();
-            switch(shapeColor.Next(1,9))
+            switch (shapeColor.Next(1, 9))
             {
-                case 1: addSquare(Brushes.Red);
+                case 1:
+                    addSquare(Brushes.Red);
+                    //enemyHealth.Maximum = 200;
+                    //enemyHealth.Value = 200;
                     break;
-                case 2: addSquare(Brushes.Blue);
+                case 2:
+                    addSquare(Brushes.Blue);
+                    //enemyHealth.Maximum = 200;
+                    //enemyHealth.Value = 200;
                     break;
-                case 3: addSquare(Brushes.Yellow);
+                case 3:
+                    addSquare(Brushes.Yellow);
+                    //enemyHealth.Maximum = 200;
+                    //enemyHealth.Value = 200;
                     break;
-                case 4: addCircle(Brushes.Red);
+                case 4:
+                    addCircle(Brushes.Red);
+                    //enemyHealth.Maximum = 100;
+                    //enemyHealth.Value = 100;
                     break;
-                case 5: addCircle(Brushes.Blue);
+                case 5:
+                    addCircle(Brushes.Blue);
+                    //enemyHealth.Maximum = 100;
+                    //enemyHealth.Value = 100;
                     break;
-                case 6: addCircle(Brushes.Yellow);
+                case 6:
+                    addCircle(Brushes.Yellow);
+                    //enemyHealth.Maximum = 100;
+                    //enemyHealth.Value = 100;
                     break;
-                case 7: addTriangle(Brushes.Blue);
+                case 7:
+                    addTriangle(Brushes.Blue);
+                    //enemyHealth.Maximum = 150;
+                    //enemyHealth.Value = 150;
                     break;
-                case 8:addTriangle(Brushes.Yellow);
+                case 8:
+                    addTriangle(Brushes.Yellow);
+                    //enemyHealth.Maximum = 150;
+                    //enemyHealth.Value = 150;
                     break;
-                default: addTriangle(Brushes.Red);
+                default:
+                    addTriangle(Brushes.Red);
+                    //enemyHealth.Maximum = 150;
+                    //enemyHealth.Value = 150;
                     break;
             }
             fightCanv.Visibility = Visibility.Visible;
@@ -144,23 +178,41 @@ namespace Final
         {
             MessageBox.Show("used quick attack");
             enemyHealth.Value -= 10;
-            if(enemyHealth.Value <= 0)
-            {
-                MessageBox.Show("you win!");
-                return;
-            }
-            enemyAttack();
-        }
-
-        private void special_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("used special");
-            enemyHealth.Value -= 30;
             if (enemyHealth.Value <= 0)
             {
                 endScreen(true);
                 return;
             }
+            enemyAttack();
+        }
+    }
+        public int specialUses = 2;
+        private void special_Click(object sender, RoutedEventArgs e)
+        {
+            if (specialUses > 1)
+            {
+                specialUses--;
+                MessageBox.Show("used special ("+specialUses+" Uses left)");
+                enemyHealth.Value -= 30;
+                if (enemyHealth.Value <= 0)
+                {
+                    endScreen(true);
+                    return;
+                }
+            }
+            else
+            {
+                specialUses--;
+                MessageBox.Show("used special (" + specialUses + " Uses left)");
+                enemyHealth.Value -= 30;
+                if (enemyHealth.Value <= 0)
+                {
+                    endScreen(true);
+                    return;
+                }
+                special.IsEnabled = false;
+            }
+
             enemyAttack();
             enemyAttack();
         }
@@ -168,29 +220,34 @@ namespace Final
         public void enemyAttack()
         {
             Random rand = new Random();
-            if(rand.Next(1, 10) >= 3)
+            if (rand.Next(1, 10) >= 3 && rand.Next(1, 10) <= 8)
             {
                 MessageBox.Show("Enemy used quick attack");
                 playerHealth.Value -= 10;
-            } else
+            }
+            else if(rand.Next(1, 10) <= 3)
             {
                 MessageBox.Show("Enemy used Special Attack");
                 playerHealth.Value -= 30;
 
+            } else
+            {
+                MessageBox.Show("Enemy used Heal");
+                enemyHealth.Value += 20;
             }
-            if (playerHealth.Value <= 0)
+            if(playerHealth.Value<= 0)
             {
                 endScreen(false);
-                return;
             }
         }
 
         public void endScreen(bool won)
         {
-            if(won == true)
+            if (won == true)
             {
                 endText.Content = "You Win!";
-            } else
+            }
+            else
             {
                 endText.Content = "You Loose!";
             }
@@ -198,6 +255,29 @@ namespace Final
             mainCanvas.Visibility = Visibility.Hidden;
             fightCanv.Visibility = Visibility.Hidden;
             endText.Visibility = Visibility.Visible;
+            playAgain.Visibility = Visibility.Visible;
+        }
+        public int uses = 2;
+        private void Heal_Click(object sender, RoutedEventArgs e)
+        {
+            if (uses > 1) {
+                uses--;
+                MessageBox.Show("used heal (" + uses + " uses left)");
+                playerHealth.Value += 20;
+            } else
+            {
+                uses--;
+                MessageBox.Show("used heal (" + uses + " uses left)");
+                playerHealth.Value += 20;
+                heal.IsEnabled = false;
+            }
+            enemyAttack();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
         }
     }
 }
